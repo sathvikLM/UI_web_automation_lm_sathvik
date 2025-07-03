f17096c0db36659fbccf35c5c51d61eefec96940
Vidya Hampiholi <vidya.hampiholi@lightmetrics.co>
2025-07-03 14:16:11 +0530

    Update BaseClass.py

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -1,5 +1,6 @@
 import inspect
 import logging
+import os
 
 import pytest
 from selenium.webdriver.common.by import By
@@ -12,26 +13,26 @@ from selenium.webdriver.support.select import Select
 class BaseClass:
 
     def getLogger(self):
-    loggerName = inspect.stack()[1][3]
-    logger = logging.getLogger(loggerName)
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
 
-    # Ensure Logs directory exists before creating the logfile
-    logs_dir = './Logs'
-    if not os.path.exists(logs_dir):
-        os.makedirs(logs_dir)
+        # Ensure Logs directory exists before creating the logfile
+        logs_dir = './Logs'
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
 
-    fileHandler = logging.FileHandler(f'{logs_dir}/logfile.log')
-    formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
-    fileHandler.setFormatter(formatter)
+        fileHandler = logging.FileHandler(f'{logs_dir}/logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
 
-    logger.addHandler(fileHandler) 
-    logger.setLevel(logging.DEBUG)
-    return logger
+        logger.addHandler(fileHandler)
+        logger.setLevel(logging.DEBUG)
+        return logger
 
     def verifyLinkPresence(self, text):
         element = WebDriverWait(self.driver, 10).until(
-        EC.presence_of_element_located((By.LINK_TEXT, text)))
+            EC.presence_of_element_located((By.LINK_TEXT, text)))
 
-    def selectOptionByText(self,locator,text):
+    def selectOptionByText(self, locator, text):
         sel = Select(locator)
         sel.select_by_visible_text(text)

31f04f037e1b2a0d3ee50e23048ac4578b64d8f4
Vidya Hampiholi <vidya.hampiholi@lightmetrics.co>
2025-07-03 13:45:04 +0530

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -50,10 +50,12 @@ def setup(request):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         # driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=Service("/home/user/Downloads/chromedriver-linux64/chromedriver"))
+	driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        #driver = webdriver.Chrome(service=Service("/home/user/Downloads/chromedriver-linux64/chromedriver"))
         # driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
     elif browser_name == "firefox":
-        driver = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
+        #driver = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
+	driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     elif browser_name == "IE":
         print("IE driver")
     driver.get("https://admin.lightmetrics.co/")

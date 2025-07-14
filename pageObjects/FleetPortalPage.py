4ffdf27049a8e3ed46a1b346084b7de7848e0917
Vidya Hampiholi <vidya.hampiholi@lightmetrics.co>
2025-07-14 14:29:11 +0530

    Update FleetPortalPage.py

--- pageObjects/FleetPortalPage.py
+++ pageObjects/FleetPortalPage.py
@@ -34,9 +34,10 @@ class FleetPortalPage(BaseClass):
     event_summary = (By.XPATH, "(//div[@class='container']/div//div/h3)[4]")
     event_trend = (By.XPATH, "(//div[@class='container']/div//div/h3)[5]")
     # SAFETY EVENTS
-    #toggle_menu = (By.XPATH, "//button[.//mat-icon[normalize-space(.)='menu']]")
-    toggle_menu = (By.XPATH, "//mat-toolbar//button[.//mat-icon[normalize-space() = 'menu'] and not(contains(@class,'mat-mdc-icon-button-hidden'))]")
-    side_menu = (By.XPATH, "//div[contains(@class, 'mat-drawer-inner-container')]")
+    toggle_menu = (By.XPATH, "//button[.//mat-icon[normalize-space(.)='menu']]")
+    #toggle_menu = (By.XPATH, "//mat-toolbar//button[.//mat-icon[normalize-space() = 'menu'] and not(contains(@class,'mat-mdc-icon-button-hidden'))]")
+    #side_menu = (By.XPATH, "//div[contains(@class, 'mat-drawer-inner-container')]")
+    side_menu = (By.XPATH, "//mat-nav-list[contains(@class, 'side-menu')]")
     safety_events_btn = (By.XPATH, "//span[text()='Safety Events']/ancestor::span/parent::a")
     next_btn = (By.XPATH, "//span[text()='Next']")
     done_btn = (By.XPATH, "//span[text()='Done']")

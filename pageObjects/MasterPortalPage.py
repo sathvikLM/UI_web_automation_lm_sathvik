e7e62082b3e18abcbc57736db07b0b7b5e7321e2
Vidya Hampiholi <vidya.hampiholi@lightmetrics.co>
2025-07-14 13:13:41 +0530

    Update MasterPortalPage.py

--- pageObjects/MasterPortalPage.py
+++ pageObjects/MasterPortalPage.py
@@ -27,7 +27,8 @@ class MasterPortalPage(BaseClass):
     #account_search = (By.XPATH, "//input[@data-placeholder='Search account']")
     #fleetdashboard = (By.XPATH, "//button[@mattooltip='Launch fleet dashboard']")
     fleetdashboard = (By.XPATH, "//button[@mattooltip='Launch fleet dashboard']")
-    toggle_menu = (By.XPATH, "//button[.//mat-icon[normalize-space(.)='menu']]")
+    #toggle_menu = (By.XPATH, "//button[.//mat-icon[normalize-space(.)='menu']]")
+    toggle_menu = (By.XPATH, "//mat-toolbar//button[.//mat-icon[normalize-space() = 'menu'] and not(contains(@class,'mat-mdc-icon-button-hidden'))]")
     side_menu = (By.XPATH, "//div[contains(@class, 'mat-drawer-inner-container')]")
     
 

fcfd30ece658929bc9052bd6291decfc7dae06e1
Vidya Hampiholi <vidya.hampiholi@lightmetrics.co>
2025-07-15 15:48:25 +0530

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -5,7 +5,6 @@ import pytest
 import os
 import time
 from slugify import slugify
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
